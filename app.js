9d3d29413ab75407c34b25a0e2a9925f4baf80f3
FrankynSM <101581082+FrankynSM@users.noreply.github.com>
2022-06-06 19:12:57 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// Crear variables
+// Variables con var
+// var aprendiendo = 'JavaScript'
+// aprendiendo = true
+
+// Variables con const
+// const aprendiendo = 'JavaScript'
+// aprendiendo = true
+
+// console.log(aprendiendo)
+
+// Variables con let
+let aprendiendo = 'JavaScript'
+aprendiendo = true
+console.log(aprendiendo)
